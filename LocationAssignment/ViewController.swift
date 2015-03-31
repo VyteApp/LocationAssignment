e5e08cbf41fff22c3d16eb7597b91f4e35c7d185
mpertierra <marcosp@mit.edu>
2015-03-31 03:40:55 -0400

    LocationAssignment App can follow user's location and displays it on map (TODO: Log location data to file)

--- LocationAssignment/ViewController.swift
+++ LocationAssignment/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var mainLabel : UILabel!
+    @IBOutlet var startLoggingButton : UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startLoggingButtonTapped(sender : AnyObject){
+        //TODO: Log location(lat/long) and accuracy data to file
+        
+        //let documentDirectoryURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! as NSURL
+        //let randFile = "log"+String(Int(arc4random_uniform(7)))+".txt"
+        //let fileDestinationUrl = documentDirectoryURL.URLByAppendingPathComponent(randFile)
+        //let currentLocation = mapView.userLocation.location
+        //TODO: Need accuracy value
+        //let locationData = currentLocation.description
+        //locationData.writeToURL(fileDestinationUrl, atomically: true, encoding: NSUTF8StringEncoding, error: nil)
+    }
+    
+    func zoomIn() {
+        let userLocation = mapView.userLocation
+        
+        let region = MKCoordinateRegionMakeWithDistance(
+            userLocation.location.coordinate, 50, 50)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation
+        userLocation: MKUserLocation!) {
+            zoomIn()
+            //mapView.centerCoordinate = userLocation.location.coordinate
+    }
+    
 
 }
 
